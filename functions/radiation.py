da2a57a42953f0c9bce38073db87e025543af48d
Vladislav Khobta <vhobta@film-direction.com>
2023-05-05 19:31:43 +0300

    ...

--- functions/radiation.py
+++ functions/radiation.py
@@ -1,14 +1,21 @@
 from math import cos, sin
+import requests
+from pprint import pprint
+
+API = 'https://power.larc.nasa.gov'
 
 
 def calculate_radiation(beta, gamma, delta, phi, omega):
 
-    cos_theta = sin(delta) * sin(phi) * cos(beta)
-    - sin(delta) * cos(phi) * sin(beta) * cos(gamma)
-    + cos(delta) * cos(phi) * cos(beta) * cos(omega)
-    + cos(delta) * sin(phi) * sin(beta) * cos(gamma) * cos(omega)
-    + cos(delta) * sin(beta) * sin(gamma) * sin(omega)
+    ro = 0.367
+
+    # i_direct = i_flat_direct * cos(theta_z) / theta
+    # i_scattered = i_flat_scattered * (1 + cos(beta)) / 2
+    # i_reflected = ro * i_flat_reflected * (1 - cos(beta)) / 2
+
+    # insolation = i_direct + i_scattered + i_reflected
 
 
-
-    return cos_theta
+if __name__ == '__main__':
+    response = requests.get(API + '/api/application/indicators/point?longitude=-84.43&latitude=33.64&start=2015&end=2021&format=JSON')
+    pprint(response.json())
